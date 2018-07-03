efc45a4fd756d649097fd2f7f4098ce33214c644
Noman46 <nomanibrahim7@gmail.com>
2018-07-04 00:33:52 +0600

    Method signature practice

---
+++ src/noman/practice/methodSignature/methodSignature.java
@@ -0,0 +1,25 @@
+
+package noman.practice.methodSignature;
+
+/**
+ *
+ * @author Noman Ibrahim
+ */
+public class methodSignature {
+ /*
+    Tyhe method name m1 and the argument type "int" togather called method signature
+    */
+   public void m1(int i){
+       System.out.println("method m1");
+       }
+   /*
+    A single class can not have two methods having same method signature
+   */
+    // public int m1(int i){
+    //System.out.println("method m1");
+    //}
+    public static void main(String[] args) {
+       
+    }
+    
+}
